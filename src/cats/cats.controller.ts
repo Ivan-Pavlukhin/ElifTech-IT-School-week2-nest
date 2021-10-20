bc578ae06f30427799f3c3496d67d2e9bf92a470
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-10-20 17:07:58 +0300

    Fix cats.service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { map, Observable } from 'rxjs';
 import { CatsService } from './cats.service';
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -7,9 +7,11 @@ import { Observable } from 'rxjs';
 export class CatsService {
   constructor(private readonly httpService: HttpService) {}
 
+  private API_KEY = '9a3464e6-e7be-4fff-a36b-d8a5267cd49e';
+
+  private BASS_URL = `https://api.thecatapi.com/v1/breeds/search?x-api-key=${this.API_KEY}`;
+
   getCats(breed_name): Observable<AxiosResponse<any>> {
-    return this.httpService.get(
-      `https://api.thecatapi.com/v1/breeds/search?x-api-key=9a3464e6-e7be-4fff-a36b-d8a5267cd49e&q=${breed_name}`,
-    );
+    return this.httpService.get(`${this.BASS_URL}&q=${breed_name}`);
   }
 }
